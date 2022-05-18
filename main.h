bff054fc4c6573300ec119e2b4f520833483d746
Dewinsy <mushdaisy71@gmail.com>
2022-05-18 12:29:01 +0300

    Update 1

--- main.h
+++ main.h
@@ -1,6 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef SIMPLE_SHELL
+#define SIMPLE_SHELL
 
-char **spltstr(char *s);
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int _strlen(const char *s);
+char *_strcpy(char *dest, const char *src);
+int _putchar(char c);
+void _puts(char *str);
+unsigned int nbr_spaces(char *s);
+char **stringToTokens(char *str);
 
 #endif
